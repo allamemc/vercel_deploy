9666a3637b9523716b9a59fcd8ae0d81f4c3fea5
allamemc <allamemc@gmail.com>
2023-11-06 10:15:47 +0100

    dwadwa

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// index.js
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 4000;
 
+app.use(express.static(path.join(__dirname, "public"))); // Configura la carpeta "public" como estática
+
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+  console.log(`API listening on PORT ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hola desde vercel");
+  res.sendFile(path.join(__dirname, "public", "index.html")); // Sirve el archivo index.html
 });
 
-// Export the Express API
+// Exporta la API de Express
 module.exports = app;
